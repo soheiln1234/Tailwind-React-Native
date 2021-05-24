bbbd7554ccaf3f4ada821bd99e47e8a49f9ce199
Soheil Nikroo <soheiln1234@gmail.com>
2021-05-24 09:06:45 +0430

    Resize the view.

--- components/Tailwind.js
+++ components/Tailwind.js
@@ -6,7 +6,7 @@ const Tailwind = () => {
     return (
         <SafeAreaView style={{...tailwind('h-full'),...styles.bg}}>
 		<View style={tailwind('pt-12 items-center justify-center flex-1')}>
-			<View style={tailwind('bg-blue-200 px-3 py-1 rounded-full justify-center items-center')}>
+			<View style={tailwind('bg-blue-200 px-3 py-1 rounded-full justify-center items-center w-1/2 h-1/4')}>
 				<Text style={tailwind('text-blue-900 font-bold')}>
 					Hello Tailwind
 				</Text>
